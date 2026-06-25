8c44347582e60fcaf74f60824d37f893a356c6e0
ghost <www.sherm@yandex.ru>
2026-06-25 17:54:55 +0300

    multithreading added

--- client_project/client.cpp
+++ client_project/client.cpp
@@ -1,96 +1,101 @@
-#include <cstring>
-#include <iostream>
-#include <string>
-
 #include <arpa/inet.h>
 #include <netinet/in.h>
 #include <sys/socket.h>
 #include <unistd.h>
 
+#include <cstring>
+#include <iostream>
+#include <string>
+
 // клиент
 
 struct Command {
-  int massage{-1};
+    int massage{-1};
 };
 
 Command client;
 
 class Client {
-private:
-  int clientSock;
+   private:
+    int clientSock;
 
-public:
-  Client() : clientSock(-1) {}
+   public:
+    Client() : clientSock(-1) {}
 
-  ~Client() { close(this->clientSock); }
+    ~Client() { close(this->clientSock); }
 
-  void StartClient(short port) {
-    this->clientSock = socket(AF_INET, SOCK_STREAM, 0);
-    if (this->clientSock < 0) {
-      std::cerr << "Ошибка создания сокета клиента " << strerror(errno);
-      exit(1);
+    void StartClient(short port) {
+        this->clientSock = socket(AF_INET, SOCK_STREAM, 0);
+        if (this->clientSock < 0) {
+            std::cerr << "Ошибка создания сокета клиента " << strerror(errno);
+            exit(1);
+        }
+
+        std::cout << "Сокет клиента успешно создан." << std::endl;
+
+        struct sockaddr_in clientAddress{};
+        clientAddress.sin_family = AF_INET;
+        clientAddress.sin_port = htons(port);
+        if (inet_pton(AF_INET, "127.0.0.1", &clientAddress.sin_addr.s_addr) < 0) {
+            std::cerr << "Указа неверный IP адрес. " << strerror(errno);
+            exit(1);
+        }
+
+        socklen_t socklen = sizeof(clientAddress);
+        if (connect(this->clientSock, (struct sockaddr*)&clientAddress, socklen) < 0) {
+            std::cerr << "Ошибка подключения к серверу " << strerror(errno);
+            exit(1);
+        }
+
+        std::cout << "Успешное подключение к серверу." << std::endl;
+    }
+    void send_command() {
+        std::cout << "Введите конанду\n";
+        while (true) {
+            std::cout << "> ";
+            std::string input;
+            std::getline(std::cin, input);
+
+            try {
+                client.massage = std::stoi(input);
+            } catch (const std::exception& e) {
+                std::cout << "Ошибка! Введите целое число.\n";
+                continue;
+            }
+
+            if (send(this->clientSock, &client.massage, sizeof(client.massage), 0) < 0) {
+                std::cerr << "Ошибка. Данные не были отправлены" << strerror(errno);
+                exit(1);
+            }
+
+            std::cout << "Данные отправлены успешно." << std::endl;
+            if (recv_command() == 0) {
+                break;
+            }
+        }
     }
 
-    std::cout << "Сокет клиента успешно создан." << std::endl;
+    int recv_command() {
+        std::cout << "Получение ответа от сервера" << std::endl;
+        int bite_size = recv(this->clientSock, &client.massage, sizeof(client.massage), 0);
+        std::cout << "Получена команда от сервера: " << client.massage << std::endl;
 
-    struct sockaddr_in clientAddress{};
-    clientAddress.sin_family = AF_INET;
-    clientAddress.sin_port = htons(port);
-    if (inet_pton(AF_INET, "127.0.0.1", &clientAddress.sin_addr.s_addr) < 0) {
-      std::cerr << "Указа неверный IP адрес. " << strerror(errno);
-      exit(1);
+        if (bite_size == 0) {
+            std::cout << "Сервер штатно закрыл соединение" << std::endl;
+            return 0;
+        } else if (bite_size == -1) {
+            std::cerr << "Возникла ошибка при принятии данных от сервера" << strerror(errno);
+            exit(1);
+        }
+        return 1;
     }
-
-    socklen_t socklen = sizeof(clientAddress);
-    if (connect(this->clientSock, (struct sockaddr *)&clientAddress, socklen) <
-        0) {
-      std::cerr << "Ошибка подключения к серверу " << strerror(errno);
-      exit(1);
-    }
-
-    std::cout << "Успешное подключение к серверу." << std::endl;
-  }
-  void send_command() {
-    std::cout << "Введите конанду\n";
-    while (true) {
-      std::cout << "> ";
-      std::cin >> client.massage;
-      if (send(this->clientSock, &client.massage, sizeof(client.massage), 0) <
-          0) {
-        std::cerr << "Ошибка. Данные не были отправлены" << strerror(errno);
-        exit(1);
-      }
-
-      std::cout << "Данные отправлены успешно." << std::endl;
-      if (recv_command() == 0) {
-        break;
-      }
-    }
-  }
-
-  int recv_command() {
-    std::cout << "Получение ответа от сервера" << std::endl;
-    int bite_size =
-        recv(this->clientSock, &client.massage, sizeof(client.massage), 0);
-    std::cout << "Получена команда от сервера: " << client.massage << std::endl;
-
-    if (bite_size == 0) {
-      std::cout << "Сервер штатно закрыл соединение" << std::endl;
-      return 0;
-    } else if (bite_size == -1) {
-      std::cerr << "Возникла ошибка при принятии данных от сервера"
-                << strerror(errno);
-      exit(1);
-    }
-    return 1;
-  }
 };
 
-int main(int argc, char *argv[]) {
-  Client client;
+int main(int argc, char* argv[]) {
+    Client client;
 
-  client.StartClient(std::stoi(argv[1]));
-  client.send_command();
+    client.StartClient(std::stoi(argv[1]));
+    client.send_command();
 
-  return 0;
+    return 0;
 }
--- server_project/server.cpp
+++ server_project/server.cpp
@@ -1,122 +1,154 @@
-#include <cstring>
-#include <iostream>
-#include <string>
-#include <vector>
-
 #include <arpa/inet.h>
 #include <netinet/in.h>
 #include <sys/socket.h>
 #include <unistd.h>
 
+#include <cstring>
+#include <iostream>
+#include <mutex>
+#include <string>
+#include <thread>
+#include <vector>
+
 // сервер
 
 struct Command {
-  int commandType = 1;
+    int commandType{-1};
 };
 
-Command command;
+std::mutex mtx_client;
+
+void handle_client(int client_fd) {
+    // функция для обработки каждого киента в отдельном потоке
+    {
+        std::lock_guard<std::mutex> lockend(mtx_client);
+        std::cout << "Клиент подключился. (Поток: " << std::this_thread::get_id() << ")\n";
+    }
+
+    Command command;
+    while (true) {
+        int bytes = recv(client_fd, &command.commandType, sizeof(command.commandType), 0);
+
+        if (bytes <= 0) {
+            {
+                std::lock_guard<std::mutex> lockend(mtx_client);
+                if (bytes == 0) {
+                    std::cout << "Клиент штатно закрыл соединение. (Поток: " << std::this_thread::get_id()
+                              << ")" << std::endl;
+                } else {
+                    std::cout << "Ошибка при прочтении данных от клиента. Данные не прочитаны. (Поток: "
+                              << std::this_thread::get_id() << ") " << strerror(errno);
+                }
+            }
+            break;
+        }
+        {
+            std::lock_guard<std::mutex> lockend(mtx_client);
+            std::cout << "Данные успешно прочитаны!\nПоступила команда: " << command.commandType << "\n";
+        }
+
+        if (send(client_fd, &command.commandType, sizeof(command.commandType), 0) < 0) {
+            {
+                std::lock_guard<std::mutex> lockend(mtx_client);
+                std::cout << "Ошибка отправки данных клиенту. (Поток: " << std::this_thread::get_id() << ") "
+                          << strerror(errno);
+            }
+            break;
+        }
+        {
+            std::lock_guard<std::mutex> lockend(mtx_client);
+            std::cout << "Данные успешно отправлены клиенту! Команда: " << command.commandType
+                      << " (Поток: " << std::this_thread::get_id() << ")\n";
+        }
+    }
+    close(client_fd);
+    {
+        std::lock_guard<std::mutex> lockend(mtx_client);
+        std::cout << "Клиент успешно бработан и закрыт! (Поток: " << std::this_thread::get_id() << ")"
+                  << std::endl;
+    }
+}
 
 class Server {
-private:
-  int serverSock;
-  int clientSock;
+   private:
+    int serverSock;
+    bool runing;
 
-public:
-  Server() : serverSock(-1), clientSock(-1) {}
-  ~Server() {}
-
-  void StartServer() {
-    this->serverSock = socket(AF_INET, SOCK_STREAM, 0);
-    if (this->serverSock < 0) {
-      std::cerr << "Ошибка создания сокета\n";
-      exit(1);
+   public:
+    Server() : serverSock(-1), runing(false) {}
+    ~Server() {
+        if (runing) close_command();
     }
 
-    struct sockaddr_in serverAddress{};
-    serverAddress.sin_family = AF_INET;
-    serverAddress.sin_port = htons(8080);
-    // serverAddress.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
-    if (inet_pton(AF_INET, "127.0.0.1", &serverAddress.sin_addr.s_addr) < 0) {
-      std::cerr << "Указан неверный IP адрес. " << strerror(errno);
-      exit(1);
+    void StartServer() {
+        this->serverSock = socket(AF_INET, SOCK_STREAM, 0);
+        if (this->serverSock < 0) {
+            std::cerr << "Ошибка создания сокета\n";
+            exit(1);
+        }
+
+        struct sockaddr_in serverAddress{};
+        serverAddress.sin_family = AF_INET;
+        serverAddress.sin_port = htons(8080);
+        // serverAddress.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
+        if (inet_pton(AF_INET, "127.0.0.1", &serverAddress.sin_addr.s_addr) < 0) {
+            std::cerr << "Указан неверный IP адрес. " << strerror(errno);
+            exit(1);
+        }
+
+        if (bind(this->serverSock, (struct sockaddr*)&serverAddress, sizeof(serverAddress)) < 0) {
+            std::cerr << "Ошибка привязки сокета к адресу и порту. " << strerror(errno);
+            exit(1);
+        }
+
+        std::cout << "Сервер запущен к порту: " << ntohs(serverAddress.sin_port) << "\n";
+
+        if (listen(this->serverSock, 1) < 0) {
+            std::cerr << "Ошибка запуска прослушивания входящих соединений. " << strerror(errno);
+            exit(1);
+        }
+
+        std::cout << "Ожидание подключения клиента..." << std::endl;
+
+        this->runing = true;
     }
 
-    if (bind(this->serverSock, (struct sockaddr *)&serverAddress,
-             sizeof(serverAddress)) < 0) {
-      std::cerr << "Ошибка привязки сокета к адресу и порту. "
-                << strerror(errno);
-      exit(1);
+    void run() {
+        while (this->runing) {
+            struct sockaddr_in clientAddress{};
+            socklen_t clientAddressLen = sizeof(clientAddress);
+
+            int clientSock = accept(this->serverSock, (struct sockaddr*)&clientAddress, &clientAddressLen);
+            if (clientSock < 0) {
+                if (this->runing == false)
+                    std::cerr << "Ошибка при принятии входящего соединения. " << strerror(errno);
+                continue;
+            }
+            char client_ip[INET_ADDRSTRLEN];
+            inet_ntop(AF_INET, &clientAddress.sin_addr, client_ip, INET_ADDRSTRLEN);
+            std::cout << "Подключен новый клиент: " << client_ip << "\t:\t" << ntohs(clientAddress.sin_port)
+                      << std::endl;
+
+            std::thread client_thread(handle_client, clientSock);
+            client_thread.detach();
+        }
     }
 
-    std::cout << "Сервер запущен." << std::endl;
+    void close_command() {
+        this->runing = false;
+        shutdown(this->serverSock, 2);
 
-    if (listen(this->serverSock, 1) < 0) {
-      std::cerr << "Ошибка запуска прослушивания входящих соединений. "
-                << strerror(errno);
-      exit(1);
+        if (this->serverSock != -1) {
+            close(this->serverSock);
+            this->serverSock = -1;
+        }
     }
-
-    std::cout << "Ожидание подключения клиента к порту: "
-              << ntohs(serverAddress.sin_port) << std::endl;
-
-    struct sockaddr_in clientAddress{};
-    socklen_t clientAddressLen = sizeof(clientAddress);
-
-    this->clientSock = accept(
-        this->serverSock, (struct sockaddr *)&clientAddress, &clientAddressLen);
-    if (this->clientSock < 0) {
-      std::cerr << "Ошибка при принятии входящего соединения. "
-                << strerror(errno);
-      exit(1);
-    }
-
-    std::cout << "Клиет успешно подключен!" << std::endl;
-  }
-
-  void send_command() {
-    if (send(this->clientSock, &command.commandType,
-             sizeof(command.commandType), 0) < 0) {
-      std::cerr << "Ошибка при отправке данных клиенту. " << strerror(errno);
-      exit(1);
-    }
-    std::cout << "Данные успешно отправлены клиенту!" << std::endl;
-  }
-
-  void recv_command() {
-    if (recv(this->clientSock, &command.commandType,
-             sizeof(command.commandType), 0) < 0) {
-      std::cerr
-          << "Ошибка при прочтении данных от клиента. Данные не прочитаны... "
-          << strerror(errno);
-      exit(1);
-    }
-
-    std::cout << "Данные успешно прочитаны!\nПоступила команда: "
-              << command.commandType << std::endl;
-  }
-
-  void close_command() {
-    shutdown(this->serverSock, 2);
-    shutdown(this->clientSock, 2);
-
-    if (this->serverSock != -1) {
-      close(this->serverSock);
-      this->serverSock = -1;
-    }
-    if (this->clientSock != -1) {
-      close(this->clientSock);
-      this->clientSock = -1;
-    }
-  }
 };
 
-int main(int argc, char *argv[]) {
-  Server server;
-  server.StartServer();
-  server.recv_command();
-  // server.send_command();
+int main(int argc, char* argv[]) {
+    Server server;
+    server.StartServer();
+    server.run();
 
-  server.close_command();
-
-  return 0;
+    return 0;
 }
